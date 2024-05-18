cbddefe3ee29f96de369258bc53db9deb4a2d86b
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-05-19 01:55:39 +0800

    git subrepo pull Skritt
    subrepo:
      subdir:   "Skritt"
      merged:   "b6f8ce5"
    upstream:
      origin:   "https://github.com/hojin-koh/zsh-Skritt"
      branch:   "main"
      commit:   "b6f8ce5"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

--- Skritt/lib/msg.zsh
+++ Skritt/lib/msg.zsh
@@ -17,21 +17,25 @@ typeset -F SECONDS
 opt -Skritt debug false "Whether to show debug messages on screen"
 
 __::outputMessage() {
-  local typ="$1"
-  local color="$2"
-  local msg="$3"
-  local nl="${4-\n}"
-  local t="$SECONDS"
-  printf "\033[%sm[%s-%06.1f] %s\033[m$nl" "$color" "$typ" "$t" "$msg" >&5
-  printf "[%s-%06.1f] %s$nl" "$typ" "$t" "$msg" >&6
+  local __typ="$1"
+  local __color="$2"
+  local __msg="$3"
+  local __nl="${4-\n}"
+  local __t="$SECONDS"
+  printf "\033[%sm[%s-%08.1f] %s\033[m\033[K$__nl" "$__color" "$__typ" "$__t" "$__msg" >&5
+  printf "[%s-%08.1f] %s$__nl" "$__typ" "$__t" "$__msg" >&6
 }
 
 debug() {
   if [[ "${debug-}" == true ]]; then
-    printf "\033[0;34m[D-%06.1f] %s\033[m\n" "$SECONDS" "$1" >&2
+    printf "\033[0;34m[D-%06.1f] %s\033[m\033[K\n" "$SECONDS" "$1" >&2
   else
     printf "[D-%06.1f] %s\n" "$SECONDS" "$1" >&6
   fi
+}
+
+titleinfo() {
+  __::outputMessage T '1;32' "$1"
 }
 
 info() {
@@ -60,4 +64,21 @@ promptyn() {
   read -q REPLY || true
   echo >&5
   printf "[%06.1f] REPLY=%s\n" "$SECONDS" "$REPLY" >&6
+}
+
+showReadableTime() {
+  local __secTotal="$1"
+  local __hours=$[__secTotal/3600]
+  local __minutes=$[(__secTotal%3600)/60]
+  local __seconds=$[__secTotal%60]
+  if [[ $__hours == 0 ]]; then
+    printf "%dm%ds" $__minutes $__seconds
+  else
+    printf "%dh%dm%ds" $__hours $__minutes $__seconds
+  fi
+}
+
+lineProgressBar() {
+  local __max="$1"
+  pv -l -F "%t %b/$__max %p %e" -i 2 -s $__max
 }

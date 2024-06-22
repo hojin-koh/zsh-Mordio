643e7da66c18a360887dab6a8aa089618c8b9a30
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-06-23 00:14:53 +0800

    git subrepo pull Skritt
    subrepo:
      subdir:   "Skritt"
      merged:   "59b136b"
    upstream:
      origin:   "https://github.com/hojin-koh/zsh-Skritt"
      branch:   "main"
      commit:   "59b136b"
    git-subrepo:
      version:  "0.4.6"
      origin:   "???"
      commit:   "???"

--- Skritt/lib/opts.zsh
+++ Skritt/lib/opts.zsh
@@ -24,45 +24,46 @@ declare -gA skrittMapOptDesc
 # Usage: opt [-r] [-<Group Name>] <opt-name> <default-value> <description>
 opt() {
   local __isRequired=false
-  if [[ "${1-}" == "-r" ]]; then
+  if [[ ${1-} == -r ]]; then
     __isRequired=true
     shift
   fi
   # A group is specified
   local __grpThis=""
-  if [[ "${1-}" == -* ]]; then
-    __grpThis="${1:1}"
+  if [[ ${1-} == -* ]]; then
+    __grpThis=${1:1}
     shift
   fi
-  local __nameOpt="$1"
-  local __nameVar="${__nameOpt//-/_}"
-  local __nameVar="${__nameVar//./___}"
-  local __valueDefault="$2"
-  local __descOpt="$3"
-  skrittOpts+=( "$__nameVar" )
+  local __nameOpt=$1
+  local __nameVar=${__nameOpt//-/_}
+  local __nameVar=${__nameVar//./___}
+  local __valueDefault=$2
+  local __descOpt=$3
+  skrittOpts+=( $__nameVar )
 
   # Write the description of this option
-  if [[ "$__isRequired" == true ]]; then
+  if [[ $__isRequired == true ]]; then
     skrittMapOptDesc[$__nameVar]="[Req]$__nameOpt=$__valueDefault"$'\t'"$__descOpt"
-    skrittRequiredArgs+=( "$__nameVar" )
+    skrittRequiredArgs+=( $__nameVar )
   else
     skrittMapOptDesc[$__nameVar]="[--]$__nameOpt=$__valueDefault"$'\t'"$__descOpt"
   fi
 
   # Take note of group, and add to group list if not yet done
-  skrittMapOptGroup[$__nameVar]="$__grpThis"
+  skrittMapOptGroup[$__nameVar]=$__grpThis
+  # [(i)...]: search for the 1-based index of this element. As per zsh doc: "On failure substitutes the length of the array plus one"
   if [[ ${skrittOptGroups[(i)$__grpThis]} -gt ${#skrittOptGroups} ]]; then
-    if [[ -n "$__grpThis" && "$__grpThis" != "Skritt" ]]; then
-      skrittOptGroups+=( "$__grpThis" )
+    if [[ -n $__grpThis && $__grpThis != Skritt ]]; then
+      skrittOptGroups+=( $__grpThis )
     fi
   fi
 
   # Make the variable and assign it the default value
-  if [[ "$__valueDefault" == "("* ]]; then
-    declare -ga "$__nameVar"
+  if [[ $__valueDefault == \(* ]]; then
+    declare -ga $__nameVar
     eval "$__nameVar=$__valueDefault"
   else
-    declare -g "$__nameVar"
+    declare -g $__nameVar
     eval "$__nameVar='$__valueDefault'"
   fi
 }
@@ -73,19 +74,23 @@ printHelpMessage() {
   printf "%s\n\n" "${description-}"
   (
     for grp in "" "${skrittOptGroups[@]}" "Skritt"; do
-      if [[ -n "$grp" ]]; then printf "\n%s Options:\n\n" "$grp"; fi
-      for var in ${(k)skrittMapOptGroup[(R)$grp]}; do
-        printf "  %s\n" "${skrittMapOptDesc[$var]}"
-      done
+      if [[ -n $grp ]]; then printf "\n%s Options:\n\n" "$grp"; fi
+      # This is less efficient than something like ${(k)skrittMapOptGroup[(R)$grp]},
+      # but doing it this way can ensure the display order is the same as decleration order
+      for var in "${skrittOpts[@]}"; do
+        if [[ ${skrittMapOptGroup[$var]-} == $grp ]]; then
+          printf "  %s\n" "${skrittMapOptDesc[$var]}"
+        fi
+      done | if command -v column >/dev/null; then column -ts $'\t'; else cat; fi
     done
-  ) | if command -v column >/dev/null; then column -tLs $'\t'; else cat; fi >&2
+  ) >&2
 }
 
 checkRequiredArgs() {
   local __var
   for __var in "${(@)skrittRequiredArgs}"; do
-    if [[ -z "${(P)__var-}" ]]; then
-      if [[ "${1+ok}" == "ok" ]]; then
+    if [[ -z ${(P)__var-} ]]; then
+      if [[ ${1+ok} == ok ]]; then
         eval "$__var='$1'"
         debug "Filled required argument \$$__var with positional argument '$1'"
         shift
@@ -100,7 +105,7 @@ addHook postparse checkRequiredArgs
 SKRITT::HOOK::logOptions() {
   local __var
   for __var in "${(@)skrittOpts}"; do
-    if [[ "${(Pt)__var-}" == "array" ]]; then
+    if [[ ${(Pt)__var-} == array ]]; then
       debug "option:$__var=(${(P*)__var-})"
     else
       debug "option:$__var=${(P)__var-}"

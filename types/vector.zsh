2913ec8d54346ac82c950259018eea5860a107c5
Hojin Koh <hojin-koh@users.noreply.github.com>
2024-06-25 20:17:28 +0800

    Grant vector the same magic as text

--- types/vector.zsh
+++ types/vector.zsh
@@ -19,9 +19,11 @@
 mordioTypeInit[vector]=MORDIO::TYPE::vector::INIT
 
 MORDIO::TYPE::vector::INIT() {
-  local nameVar="$1"
-  local inout="$2"
+  local nameVar=$1
+  local inout=$2
 
+  populateType "$nameVar" MORDIO::TYPE::file
+  populateType "$nameVar" MORDIO::TYPE::table
   populateType "$nameVar" MORDIO::TYPE::vector
 }
 
@@ -32,8 +34,6 @@ MORDIO::TYPE::vector::checkName() {
   if ! MORDIO::TYPE::file::checkName "$@"; then
     if [[ "$fname" == *.vec.zst ]]; then
       return 0
-    elif [[ "$fname" == *.npy.tar.zst ]]; then
-      return 0
     else
       err "Input argument $fname has invalid extension"
       return 36
@@ -42,75 +42,13 @@ MORDIO::TYPE::vector::checkName() {
   return 0
 }
 
-MORDIO::TYPE::vector::checkValid() {
-  MORDIO::TYPE::file::checkValid "$@"
-}
-
-MORDIO::TYPE::vector::finalize() {
-  MORDIO::TYPE::file::finalize "$@"
-}
-
-MORDIO::TYPE::vector::cleanup() {
-  MORDIO::TYPE::file::cleanup "$@"
-}
-
 MORDIO::TYPE::vector::computeMeta() {
+  local fname=$1
+  printf "[nRecord]="
+  wc -l
+  # TODO: ndim
   MORDIO::TYPE::table::computeMeta "$@"
 }
 
-MORDIO::TYPE::vector::saveMeta() {
-  MORDIO::TYPE::file::saveMeta "$@"
-}
-
-MORDIO::TYPE::vector::dumpMeta() {
-  MORDIO::TYPE::file::dumpMeta "$@"
-}
-
-MORDIO::TYPE::vector::checkScriptSum() {
-  MORDIO::TYPE::file::checkScriptSum "$@"
-}
-
-MORDIO::TYPE::vector::getMainFile() {
-  MORDIO::TYPE::file::getMainFile "$@"
-}
-
 # === Save/Load ===
-
-MORDIO::TYPE::vector::isReal() {
-  MORDIO::TYPE::file::isReal "$@"
-}
-
-MORDIO::TYPE::vector::getLoader() {
-  MORDIO::TYPE::table::getLoader "$@"
-}
-
-MORDIO::TYPE::vector::load() {
-  MORDIO::TYPE::table::load "$@"
-}
-
-MORDIO::TYPE::vector::getLoaderKey() {
-  MORDIO::TYPE::table::getLoaderKey "$@"
-}
-
-MORDIO::TYPE::vector::loadKey() {
-  MORDIO::TYPE::table::loadKey "$@"
-}
-
-MORDIO::TYPE::vector::getLoaderValue() {
-  MORDIO::TYPE::table::getLoaderValue "$@"
-}
-
-MORDIO::TYPE::vector::loadValue() {
-  MORDIO::TYPE::table::loadValue "$@"
-}
-
-MORDIO::TYPE::vector::save() {
-  MORDIO::TYPE::table::save "$@"
-}
-
-# === Metadata Processing ===
-
-MORDIO::TYPE::vector::getNR() {
-  MORDIO::TYPE::table::getNR "$@"
-}
 
